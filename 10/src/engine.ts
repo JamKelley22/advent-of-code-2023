122874d0104be92a22ee5abca2601853b2142fe2
Jameel Kelley <jkelley4@mmm.com>
2023-12-10 23:03:33 -0600

    Work on day 10 part 1

---
+++ 10/src/engine.ts
@@ -0,0 +1,156 @@
+//https://www.geeksforgeeks.org/implementation-graph-javascript/
+export class Graph<T extends string | number> {
+  AdjList: Map<T, T[]>;
+  isDirected: boolean;
+  constructor(isDirected: boolean = false) {
+    this.AdjList = new Map();
+    this.isDirected = isDirected;
+  }
+
+  addVertex(v: T) {
+    if (!this.AdjList.has(v)) this.AdjList.set(v, []);
+  }
+
+  removeVertex(v: T) {
+    if (!this.AdjList.has(v)) this.AdjList.delete(v);
+  }
+
+  addEdge(v: T, w: T) {
+    this.AdjList.get(v)?.push(w);
+    // If graph is undirected,
+    // add an edge from w to v also
+
+    if (!this.isDirected) {
+      let wVertex = this.AdjList.get(w);
+      if (!wVertex) {
+        this.addVertex(w);
+        wVertex = this.AdjList.get(w);
+      }
+      wVertex?.push(v);
+    }
+  }
+
+  printGraph() {
+    let get_keys = this.AdjList.keys();
+
+    for (let i of get_keys) {
+      let get_values = this.AdjList.get(i);
+      let conc = "";
+
+      for (let j of get_values || []) conc += j + " ";
+
+      console.log(i + " -> " + conc);
+    }
+  }
+
+  bfs(startingNode: T) {
+    // create a visited object
+    let visited: Partial<{ [key in T]: boolean }> = {};
+
+    // Create an object for queue
+    let q: T[] = []; //new Queue();
+
+    // add the starting node to the queue
+    visited[startingNode] = true;
+    q.push(startingNode);
+
+    // loop until queue is empty
+    while (q.length !== 0) {
+      // get the element from the queue
+      let getQueueElement = q.shift();
+
+      // passing the current vertex to callback function
+      console.log(getQueueElement);
+
+      // get the adjacent list for current vertex
+      if (getQueueElement) {
+        const list = this.AdjList.get(getQueueElement);
+        if (list) {
+          for (let index = 0; index < list.length; index++) {
+            const element = list[index];
+            if (!visited[element]) {
+              visited[element] = true;
+              q.push(element);
+            }
+          }
+        }
+      }
+
+      // loop through the list and add the element to the
+      // queue if it is not processed yet
+    }
+  }
+
+  dfs(startingNode: T, visit?: (vert: T, dist: number) => void) {
+    let visited: Partial<{ [key in T]: boolean }> = {};
+
+    this.DFSUtil(startingNode, visited, 0, visit);
+  }
+
+  DFSUtil(
+    vert: T,
+    visited: Partial<{ [key in T]: boolean }>,
+    dist: number,
+    visit?: (vert: T, dist: number) => void
+  ) {
+    visited[vert] = true;
+
+    visit?.(vert, dist);
+
+    let neighbors = this.AdjList.get(vert);
+    // console.log(`${vert}: `, neighbors);
+
+    if (neighbors) {
+      for (let i in neighbors) {
+        let get_elem = neighbors[i];
+        if (!visited[get_elem])
+          this.DFSUtil(get_elem, visited, dist + 1, visit);
+      }
+    }
+  }
+
+  dijkstra(startingNode: T) {
+    // Init dist map
+    const distances: Partial<{ [key in T]: number }> = {};
+    let queue: T[] = [];
+    this.AdjList.forEach((value, key) => {
+      distances[key] = Number.MAX_SAFE_INTEGER;
+      queue.push(key);
+    });
+    distances[startingNode] = 0;
+    // const visited = new Set<T>();
+
+    let count = 0;
+    while (queue.length !== 0) {
+      count++;
+
+      let minDistance = Number.MAX_SAFE_INTEGER;
+      let node: T | undefined = undefined;
+      for (let i = 0; i < queue.length; i++) {
+        const element = queue[i];
+        // if (visited.has(element)) continue;
+        if (distances[element]! <= minDistance) {
+          minDistance = distances[element]!;
+          node = element;
+        }
+      }
+      //   console.log(node);
+
+      queue = queue.filter((item) => item !== node);
+
+      if (node) {
+        const a = distances[node]!;
+        this.AdjList.get(node)?.forEach((neighbor) => {
+          const newDist = a + 1;
+          if (newDist < distances[neighbor]!) {
+            distances[neighbor] = newDist;
+          }
+        });
+      }
+
+      //   console.log("distances", distances);
+      //   if (count === 10) break;
+    }
+    return distances;
+  }
+}
---
+++ 10/src/partOne.ts
@@ -0,0 +1,193 @@
+import { Graph } from "./engine";
+
+var fs = require("fs");
+const util = require("util");
+
+// | is a vertical pipe connecting north and south.
+// - is a horizontal pipe connecting east and west.
+// L is a 90-degree bend connecting north and east.
+// J is a 90-degree bend connecting north and west.
+// 7 is a 90-degree bend connecting south and west.
+// F is a 90-degree bend connecting south and east.
+// . is ground; there is no pipe in this tile.
+// S is the starting position of the animal; there is a pipe on this tile, but your sketch doesn't show what shape the pipe has.
+
+export const Space = ["|", "-", "L", "J", "7", "F", ".", "S"] as const;
+type SpaceType = (typeof Space)[number];
+export const Direction = ["North", "South", "East", "West"] as const;
+type DirectionType = (typeof Direction)[number];
+
+function isOfTypeSpace(keyInput: string): keyInput is SpaceType {
+  return Space.includes(keyInput as SpaceType);
+}
+
+const getSpaceEncodedString = (
+  i: number,
+  j: number,
+  space: SpaceType
+): string => `[${i},${j}]-${space}`;
+
+const spaceDirMap = new Set<string>([
+  `${"|" satisfies SpaceType}-${"North" satisfies DirectionType}`,
+  `${"|" satisfies SpaceType}-${"South" satisfies DirectionType}`,
+
+  `${"-" satisfies SpaceType}-${"East" satisfies DirectionType}`,
+  `${"-" satisfies SpaceType}-${"West" satisfies DirectionType}`,
+
+  `${"L" satisfies SpaceType}-${"North" satisfies DirectionType}`,
+  `${"L" satisfies SpaceType}-${"East" satisfies DirectionType}`,
+
+  `${"J" satisfies SpaceType}-${"North" satisfies DirectionType}`,
+  `${"J" satisfies SpaceType}-${"West" satisfies DirectionType}`,
+
+  `${"7" satisfies SpaceType}-${"South" satisfies DirectionType}`,
+  `${"7" satisfies SpaceType}-${"West" satisfies DirectionType}`,
+
+  `${"F" satisfies SpaceType}-${"East" satisfies DirectionType}`,
+  `${"F" satisfies SpaceType}-${"South" satisfies DirectionType}`,
+]);
+
+type Grid = {
+  spaces: Map<string, SpaceType>;
+  graph: Graph<string>;
+};
+
+type Location = {
+  i: number;
+  j: number;
+};
+
+export const parseGridToSpaceMatrix = (
+  lines: string[]
+): { spaceMatrix: SpaceType[][]; startLoc?: Location } => {
+  let startLoc: Location | undefined = undefined;
+  const spaceMatrix = lines.map((line, i) =>
+    line.split("").reduce((acc, spaceStr, j) => {
+      if (isOfTypeSpace(spaceStr)) {
+        if (spaceStr === "S") startLoc = { i, j };
+        return [...acc, spaceStr];
+      }
+      return acc;
+    }, [] as SpaceType[])
+  );
+  return {
+    spaceMatrix,
+    startLoc,
+  };
+};
+export const parseGridToSpaceMap = (
+  spaceMatrix: SpaceType[][]
+): Map<string, SpaceType> => {
+  const map = new Map<string, SpaceType>();
+
+  for (let i = 0; i < spaceMatrix.length; i++) {
+    const spaceLine = spaceMatrix[i];
+    for (let j = 0; j < spaceLine.length; j++) {
+      const space = spaceLine[j];
+      if (space !== ".") map.set(getSpaceEncodedString(i, j, space), space);
+    }
+  }
+
+  return map;
+};
+export const parseGridToGraph = (spaceMatrix: SpaceType[][]): Graph<string> => {
+  const graph = new Graph<string>(false);
+
+  for (let i = 0; i < spaceMatrix.length; i++) {
+    const spaceLine = spaceMatrix[i];
+    for (let j = 0; j < spaceLine.length; j++) {
+      const space = spaceLine[j];
+      const spaceCoords = getSpaceEncodedString(i, j, space);
+      if (space !== ".") graph.addVertex(spaceCoords);
+      //I think we only need to look right and down since we
+      // 1. only have right angles and
+      // 2. we are traversing top to bottom and left to right and
+      // 3. the graph is undirected
+
+      // Look right and add edge
+      if (j < spaceLine.length - 1) {
+        const rightSpace = spaceMatrix[i][j + 1];
+        const spaceCoordsRight = getSpaceEncodedString(i, j + 1, rightSpace);
+        const rightSpaceConnects = spaceDirMap.has(`${rightSpace}-West`); //Opposite since we are checking right to left here
+
+        if (rightSpaceConnects) {
+          graph.addEdge(spaceCoords, spaceCoordsRight);
+          //   console.log(
+          //     `${spaceCoords},${space} right to ${spaceCoordsRight},${rightSpace}`
+          //   );
+        }
+      }
+
+      // Look down and add edge
+      if (i < spaceMatrix.length - 1) {
+        const downSpace = spaceMatrix[i + 1][j];
+        const spaceCoordsDown = getSpaceEncodedString(i + 1, j, downSpace);
+        const downSpaceConnects = spaceDirMap.has(`${downSpace}-North`); //Opposite since we are checking right to left here
+
+        if (downSpaceConnects) {
+          graph.addEdge(spaceCoords, spaceCoordsDown);
+          //   console.log(
+          //     `${spaceCoords},${space} down to ${spaceCoordsDown},${downSpace}`
+          //   );
+        }
+      }
+    }
+  }
+
+  return graph;
+};
+
+export const spaceMatrixToString = (spaceMatrix: SpaceType[][]): string => {
+  let ret = "  ";
+  [...new Array(spaceMatrix[0].length).keys()].forEach(
+    (num, i) => (ret += i + " ")
+  );
+  ret += "\n";
+  for (let i = 0; i < spaceMatrix.length; i++) {
+    const spaceLine = spaceMatrix[i];
+    ret += i + " ";
+    for (let j = 0; j < spaceLine.length; j++) {
+      const space = spaceLine[j];
+      ret += space + " ";
+    }
+    ret += "\n";
+  }
+  return ret;
+};
+
+export const printDistances = (
+  distances: Partial<{
+    [x: string]: number;
+  }>,
+  spaceMatrix: SpaceType[][]
+): string => {
+  let ret = "";
+
+  for (let i = 0; i < spaceMatrix.length; i++) {
+    const spaceLine = spaceMatrix[i];
+    for (let j = 0; j < spaceLine.length; j++) {
+      const space = spaceLine[j];
+      const dist = distances[getSpaceEncodedString(i, j, space)];
+      if (dist !== undefined) {
+        ret += dist % 10;
+        continue;
+      }
+      ret += space;
+    }
+    ret += "\n";
+  }
+
+  return ret;
+};
+
+try {
+  const useExample = true;
+  const filePath = useExample ? "input-example3.txt" : "input.txt";
+  const input = fs.readFileSync(filePath, "utf8");
+
+  const lines: string[] = input.split("\n");
+  const { spaceMatrix, startLoc } = parseGridToSpaceMatrix(lines);
+  console.log("startLoc", startLoc);
+} catch (e: any) {
+  console.log("Error:", e.stack);
+}
